fbbf8874dba5087a8ce3355e4679da9ca7e63a18
Peter Jung <peter.jung@heureka.cz>
2020-08-02 13:59:51 +0200

    Problem 14

--- src/main/scala/Problem14.scala
+++ src/main/scala/Problem14.scala
@@ -1,1 +1,34 @@
-object Problem14 extends App {}
+import scala.collection.mutable.HashMap
+
+object Problem14 extends App {
+    val upperLimit = 1000000
+
+    def next(n: BigInt) = if (n % 2 == 0) n / 2 else 3 * n + 1
+
+    var (finalNumber, finalLength) = (BigInt(0), BigInt(0))
+    var cache = HashMap[BigInt, BigInt]()
+
+    for (n <- upperLimit / 2 until upperLimit) {
+        var (result, length) = (BigInt(n), BigInt(1))
+
+        while (result != 1) {
+            if (cache.contains(result)) {
+                length += cache(result)
+                result = 1
+            } else {
+                result = next(result)
+                length += 1
+            }
+        }
+
+        cache(n) = length
+
+        if (length > finalLength) {
+            finalNumber = n
+            finalLength = length
+        }
+    }
+
+    println(finalNumber, finalLength)
+    println(finalNumber == 837799)
+}
